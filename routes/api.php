aa80217a02c6370dadc9d76b30f043528728ae83
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2023-12-22 19:11:14 +0100

    Update route into web

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::resource('users', UsersController::class);
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
